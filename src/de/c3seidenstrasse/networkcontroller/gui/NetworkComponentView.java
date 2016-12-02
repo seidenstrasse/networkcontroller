d5c644bc65cb913a7f610f8cd3ad4ce450dad42c
Jan Felix Wiebe <git@jfwie.be>
2016-12-02 09:04:41 +0100

    added currentExit to NCView

--- src/de/c3seidenstrasse/networkcontroller/gui/NetworkComponentView.java
+++ src/de/c3seidenstrasse/networkcontroller/gui/NetworkComponentView.java
@@ -1,6 +1,7 @@
 package de.c3seidenstrasse.networkcontroller.gui;
 
 import de.c3seidenstrasse.networkcontroller.network.NetworkComponent;
+import de.c3seidenstrasse.networkcontroller.utils.NoAttachmentException;
 
 public class NetworkComponentView extends CenterView {
 
@@ -9,6 +10,13 @@ public class NetworkComponentView extends CenterView {
 
 	protected void update(final NetworkComponent nc) {
 		this.title.setText(nc.toString());
+		this.left1.setText("Aktueller Ausgang: " + nc.getCurrentExit());
+		try {
+			this.left2.visibleProperty().set(true);
+			this.left2.setText("Verbunden mit: " + nc.getChildAt(nc.getCurrentExit()));
+		} catch (final NoAttachmentException e) {
+			this.left2.visibleProperty().set(false);
+		}
 	}
 
 }
