848c8ac37e58c5c45a8790974190140bd8e0e5ca
Jan Felix Wiebe <git@jfwie.be>
2016-12-02 23:15:15 +0100

    segmented center into content and button

--- src/de/c3seidenstrasse/networkcontroller/gui/CenterView.java
+++ src/de/c3seidenstrasse/networkcontroller/gui/CenterView.java
@@ -5,23 +5,32 @@ import javafx.geometry.Pos;
 import javafx.scene.control.Label;
 import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 
 public abstract class CenterView extends VBox {
 	protected final Label title;
 
-	final Label left1;
-	final Label left2;
-	final Label right1;
-	final Label right2;
+	protected final VBox content;
+	protected final HBox bottom;
+
+	protected final Label left1;
+	protected final Label left2;
+	protected final Label right1;
+	protected final Label right2;
 
 	protected CenterView() {
 		this.setPadding(new Insets(0, 20, 0, 20));
 
+		this.content = new VBox();
+		this.bottom = new HBox();
+		this.content.prefHeightProperty().bind(this.heightProperty().subtract(this.bottom.heightProperty()));
+		this.getChildren().addAll(this.content, this.bottom);
+
 		this.title = new Label("Hallo123");
 		this.title.setFont(new Font(36));
-		this.getChildren().add(this.title);
+		this.content.getChildren().add(this.title);
 
 		final GridPane subtitle = new GridPane();
 		subtitle.setGridLinesVisible(true);
@@ -30,7 +39,7 @@ public abstract class CenterView extends VBox {
 		final ColumnConstraints column2 = new ColumnConstraints();
 		column2.setPercentWidth(50);
 		subtitle.getColumnConstraints().addAll(column1, column2);
-		this.getChildren().add(subtitle);
+		this.content.getChildren().add(subtitle);
 
 		final VBox left = new VBox();
 		final VBox right = new VBox();
