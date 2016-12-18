a7e9864ca3ed0484104529bdca8f9dd4f450d9aa
Jan Felix Wiebe <git@jfwie.be>
2016-12-18 23:03:16 +0100

    added gui abstract class

---
+++ src/de/c3seidenstrasse/networkcontroller/webservice/Gui.java
@@ -0,0 +1,28 @@
+package de.c3seidenstrasse.networkcontroller.webservice;
+
+import de.c3seidenstrasse.networkcontroller.network.NetworkComponent;
+import de.c3seidenstrasse.networkcontroller.network.states.NetworkState;
+
+public abstract class Gui {
+	public abstract void updateNode(NetworkComponent nc);
+
+	public abstract void updateState(NetworkState ns);
+
+	protected class NodeChanged {
+		final String changed = "Node";
+		final NetworkComponent nc;
+
+		NodeChanged(final NetworkComponent nc) {
+			this.nc = nc;
+		}
+	}
+
+	protected class StateChanged {
+		final String changed = "State";
+		final NetworkState ns;
+
+		StateChanged(final NetworkState ns) {
+			this.ns = ns;
+		}
+	}
+}
