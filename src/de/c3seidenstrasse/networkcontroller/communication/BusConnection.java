f91b048887400ee54ef9c29cef9f30e257e6f3f1
Jan Felix Wiebe <git@jfwie.be>
2016-12-19 13:34:45 +0100

    added jna dependencies and classes for communcation

---
+++ src/de/c3seidenstrasse/networkcontroller/communication/BusConnection.java
@@ -0,0 +1,5 @@
+package de.c3seidenstrasse.networkcontroller.communication;
+
+public interface BusConnection {
+
+}
---
+++ src/de/c3seidenstrasse/networkcontroller/communication/SssConnection.java
@@ -0,0 +1,5 @@
+package de.c3seidenstrasse.networkcontroller.communication;
+
+public class SssConnection implements BusConnection {
+
+}
