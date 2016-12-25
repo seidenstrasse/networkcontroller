61d4e8e5101c2083146be388fae01b02c9e5e234
Jan Felix Wiebe <git@jfwie.be>
2016-12-25 22:22:30 +0100

    removed test content

--- src/de/c3seidenstrasse/networkcontroller/utils/NetworkStarter.java
+++ src/de/c3seidenstrasse/networkcontroller/utils/NetworkStarter.java
@@ -1,8 +1,6 @@
 package de.c3seidenstrasse.networkcontroller.utils;
 
-import de.c3seidenstrasse.networkcontroller.network.NetworkComponent;
 import de.c3seidenstrasse.networkcontroller.route.Network;
-import de.c3seidenstrasse.networkcontroller.route.Transport;
 
 public class NetworkStarter {
 
@@ -10,22 +8,6 @@ public class NetworkStarter {
 			throws NoAttachmentException, RouteNotFoundException, InterruptedException {
 		final Network n = Network.create();
 		n.getRoot().create33c3();
-		final NetworkComponent centralNode = n.getRoot().getChild();
-		final NetworkComponent gateToGo = centralNode.getChildAt(2);
-		final NetworkComponent pilz = centralNode.getChildAt(1);
-
-		final Transport t1 = new Transport(gateToGo, pilz);
-		
-		System.out.println(t1);
-
-		n.addTransport(t1);
-
-//		Thread.sleep((long) (Network.ROUTERMAXWAIT + (Math.random() * Network.DETECTIONMAXWAIT)));
-//		t1.getHighestPoint().capsulePassed();
-//		Thread.sleep((long) (Network.ROUTERMAXWAIT + (Math.random() * Network.DETECTIONMAXWAIT)));
-//		t1.getEnde().capsulePassed();
-		Thread.sleep((long) 30000);
-		t1.getEnde().capsulePassed();
 	}
 
 }
